3a6c351baf1e91d1369d059645476656feefaba5
asdis24 <asdis24@ru.is>
2026-03-27 10:26:53 +0000

    doing query 1 in part a

---
+++ Task A/query_A1.sql
@@ -0,0 +1,18 @@
+-- Query 1
+SELECT
+    OM.eining_heiti AS power_plant_source,
+    EXTRACT(YEAR FROM OM.timi) AS year,
+    EXTRACT(MONTH FROM OM.timi) AS month,
+    OM.tegund_maelingar AS measurement_type,
+    SUM(OM.gildi_kwh) AS total_kwh
+FROM raforka_legacy.orku_maelingar OM
+JOIN raforka_legacy.orku_einingar OE ON OM.eining_heiti = OE.heiti
+WHERE
+    OE.tegund = 'virkjun'   -- aðeins power plants
+    AND OM.timi >= '2025-01-01'
+    AND OM.timi < '2026-01-01'
+GROUP BY OM.eining_heiti, year, month, OM.tegund_maelingar
+ORDER BY
+    OM.eining_heiti,
+    month ASC,
+    total_kwh DESC;
