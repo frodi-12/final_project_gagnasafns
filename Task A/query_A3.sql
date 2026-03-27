f0950c15b59f47297d0f0bb1613e57ebed5462c3
frodi-12 <frodi23@ru.is>
2026-03-27 10:19:14 +0000

    doing query 3 in a

---
+++ Task A/query_A3.sql
@@ -0,0 +1,11 @@
+
+SELECT x.eining_heiti, (x.framleit_pwr-x.innmotun_pwr)/x.framleit_pwr AS Plant_to_sbu_loss_ratio, (x.framleit_pwr-x.uttekt_pwr)/x.framleit_pwr AS total_system_loss_ratio
+FROM (
+SELECT 
+        om.eining_heiti,
+        SUM(CASE WHEN om.tegund_maelingar ILIKE 'Framleiðsla' THEN gildi_kwh END) AS framleit_pwr,
+        SUM(CASE WHEN om.tegund_maelingar ILIKE 'Innmötun' THEN gildi_kwh END) AS innmotun_pwr,
+        SUM(CASE WHEN om.tegund_maelingar ILIKE 'Úttekt' THEN gildi_kwh END) AS uttekt_pwr
+    FROM raforka_legacy.orku_maelingar om
+    GROUP BY om.eining_heiti
+) AS x;
