a8e1c11c8ee3bf2c62d2974f0b4545ed3c583c2c
rakelhelga04 <rakelhelga04@gmail.com>
2026-03-27 10:48:07 +0000

    Update query_A2.sql

--- Task A/query_A2.sql
+++ Task A/query_A2.sql
@@ -1,20 +1,20 @@
 
 --query 2
 SELECT
-    eining_heiti AS power_plant_source,
-    EXTRACT(YEAR FROM timi)::int AS year,
-    EXTRACT(MONTH FROM timi)::int AS month,
-    notandi_heiti AS customer_name,
-    SUM(gildi_kwh) AS total_kwh
-FROM raforka_legacy.orku_maelingar
-WHERE timi >= '2025-01-01'
-  AND timi <  '2026-01-01'
-  AND tegund_maelingar = 'Úttekt'
+    om.eining_heiti AS power_plant_source,
+    EXTRACT(YEAR FROM om.timi)::int AS year,
+    EXTRACT(MONTH FROM om.timi)::int AS month,
+    om.notandi_heiti AS customer_name,
+    SUM(om.gildi_kwh) AS total_kwh
+FROM raforka_legacy.orku_maelingar om
+WHERE om.timi >= '2025-01-01'
+  AND om.timi <  '2026-01-01'
+  AND om.tegund_maelingar = 'Úttekt'
 GROUP BY
-    eining_heiti,
-    EXTRACT(MONTH FROM timi),
-    EXTRACT(YEAR FROM timi),
-    notandi_heiti
+    om.eining_heiti,
+    EXTRACT(MONTH FROM om.timi),
+    EXTRACT(YEAR FROM om.timi),
+    om.notandi_heiti
 ORDER BY
     power_plant_source,
     year,
