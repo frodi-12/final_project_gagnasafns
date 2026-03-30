f5dfc4d97b001efbeb9595afdf5f00ee384ddd61
asdis24 <asdis24@ru.is>
2026-03-30 14:11:30 +0000

    Update query_A1.sql

--- Task A/query_A1.sql
+++ Task A/query_A1.sql
@@ -1,15 +1,15 @@
 -- Query 1
 SELECT
-    OM.eining_heiti AS power_plant_source,
-    EXTRACT(YEAR FROM OM.timi) AS year,
-    EXTRACT(MONTH FROM OM.timi) AS month,
-    OM.tegund_maelingar AS measurement_type,
-    SUM(OM.gildi_kwh) AS total_kwh
-FROM raforka_legacy.orku_maelingar OM
-JOIN raforka_legacy.orku_einingar OE ON OM.eining_heiti = OE.heiti
+    om.eining_heiti AS power_plant_source,
+    EXTRACT(YEAR FROM om.timi) AS year,
+    EXTRACT(MONTH FROM om.timi) AS month,
+    om.tegund_maelingar AS measurement_type,
+    SUM(om.gildi_kwh) AS total_kwh
+FROM raforka_legacy.orku_maelingar om
+JOIN raforka_legacy.orku_einingar oe ON om.eining_heiti = oe.heiti
 WHERE
-    OE.tegund = 'virkjun'  
-    AND OM.timi >= '2025-01-01'
-    AND OM.timi < '2026-01-01'
-GROUP BY OM.eining_heiti, year, month, OM.tegund_maelingar
-ORDER BY OM.eining_heiti, month ASC, total_kwh DESC;
+    oe.tegund = 'virkjun'  
+    AND om.timi >= '2025-01-01'
+    AND om.timi < '2026-01-01'
+GROUP BY om.eining_heiti, year, month, om.tegund_maelingar
+ORDER BY om.eining_heiti, month ASC, total_kwh DESC;
