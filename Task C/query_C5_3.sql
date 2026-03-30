445763e233b3f53fbadc063cfd96d81213f07f73
asdis24 <asdis24@ru.is>
2026-03-30 15:33:27 +0000

    Update query_C5_3.sql

--- Task C/query_C5_3.sql
+++ Task C/query_C5_3.sql
@@ -1,6 +1,6 @@
 SELECT 
     psm.plant_ID AS power_plant_source,
     (SUM(psm.generated_pwr) - SUM(psm.received_pwr)) / SUM(psm.generated_pwr) AS plant_to_substation_loss_ratio,
-    (SUM(psm.generated_pwr) - (SELECT SUM(sumo.received_pwr) FROM public.sub_user_measurements sumo)) / SUM(psm.generated_pwr) AS total_system_loss_ratio
+    (SUM(psm.generated_pwr) - (SELECT SUM(sum.received_pwr) FROM public.sub_user_measurements sum)) / SUM(psm.generated_pwr) AS total_system_loss_ratio
 FROM public.plant_sub_measurements psm
 GROUP BY psm.plant_ID;
