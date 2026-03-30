43bad0eda10cb055fd82e0c070af4b8376629dcf
asdis24 <asdis24@ru.is>
2026-03-30 18:04:58 +0000

    m

--- Task C/query_A2_new.sql
+++ Task C/query_A2_new.sql
@@ -5,18 +5,9 @@ SELECT
     EXTRACT(MONTH FROM sum.time)::int AS month,
     SUM(sum.received_pwr) AS total_kwh
 FROM public.sub_user_measurements sum
-JOIN public.energy_user eu 
-    ON sum.energy_user_id = eu.id
-JOIN public.user_info ui 
-    ON eu.kennitala = ui.kennitala
+JOIN public.energy_user eu ON sum.energy_user_id = eu.id
+JOIN public.user_info ui ON eu.kennitala = ui.kennitala
 WHERE sum.time >= '2025-01-01'
   AND sum.time <  '2026-01-01'
-GROUP BY
-    eu.id,
-    ui.name,
-    year,
-    month
-ORDER BY
-    year,
-    month,
-    customer_name;
+GROUP BY eu.id, ui.name, year, month
+ORDER BY year, month, customer_name;
