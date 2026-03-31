75537c3e5c625fd557aa6863dce8d24c036fabba
Sindri300 <sindri300@gmail.com>
2026-03-31 18:11:17 +0000

    Update service.py

--- API/Updated api/app/services/service.py
+++ API/Updated api/app/services/service.py
@@ -41,34 +41,40 @@ TASK_C_VIEW_STATEMENTS = [
     """
     CREATE VIEW public.energy_delivered AS
         SELECT su.pwr_plant_id,
+               EXTRACT(YEAR FROM su.time) AS year,
                EXTRACT(MONTH FROM su.time) AS month,
                SUM(su.pwr_measurement_kwh) AS delivered_pwr
         FROM public.sub_user_measurements su
-        GROUP BY su.pwr_plant_id, EXTRACT(MONTH FROM su.time)
+        GROUP BY su.pwr_plant_id, EXTRACT(YEAR FROM su.time), EXTRACT(MONTH FROM su.time)
     ;
 
     CREATE VIEW public.pwr_plant_production AS
     SELECT
         p.name AS power_plant_source,
         psm.plant_id AS plant_id,
+        EXTRACT(YEAR FROM psm.time) AS year,
         EXTRACT(MONTH FROM psm.time) AS month,
         SUM(psm.pwr_measurement_kwh) FILTER (WHERE psm.type = 'Framleiðsla') AS total_production_kwh,
         SUM(psm.pwr_measurement_kwh) FILTER (WHERE psm.type = 'Innmötun') AS total_substation_pwr_kwh
     FROM public.plant_sub_measurements psm
     JOIN public.energy_unit p ON p.id = psm.plant_id
-    GROUP BY psm.plant_id, p.name, EXTRACT(MONTH FROM psm.time)
+    GROUP BY psm.plant_id, p.name, EXTRACT(YEAR FROM psm.time), EXTRACT(MONTH FROM psm.time)
     ;
 
     CREATE VIEW energy_flow AS
         SELECT ppp.power_plant_source,
+               ppp.year,
                ppp.month,
                ppp.total_production_kwh,
                ppp.total_substation_pwr_kwh,
                ed.delivered_pwr
         FROM public.pwr_plant_production ppp
         JOIN public.energy_unit eu ON eu.name = ppp.power_plant_source
-        JOIN public.energy_delivered ed ON ed.pwr_plant_id = eu.id AND ed.month = ppp.month
-        ORDER BY ppp.power_plant_source, ppp.month
+        JOIN public.energy_delivered ed
+             ON ed.pwr_plant_id = eu.id
+            AND ed.year = ppp.year
+            AND ed.month = ppp.month
+        ORDER BY ppp.power_plant_source, ppp.year, ppp.month
     ;
     """,
 ]
