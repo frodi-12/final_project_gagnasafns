04ff2c5b80819eb2ebc61a2ac5653456d26932ff
Sindri300 <sindri300@gmail.com>
2026-03-31 17:57:30 +0000

    Update service.py

--- API/Updated api/app/services/service.py
+++ API/Updated api/app/services/service.py
@@ -43,9 +43,10 @@ TASK_C_VIEW_STATEMENTS = [
         SELECT su.pwr_plant_id, EXTRACT(MONTH FROM su.time) AS month, SUM(su.pwr_measurement_kwh) AS delivered_pwr
         FROM public.sub_user_measurements su
         GROUP BY su.pwr_plant_id, EXTRACT(MONTH FROM su.time)
+    ;
 
     CREATE VIEW public.pwr_plant_production AS
-    SELECT 
+    SELECT
         p.name,
         psm.plant_id AS plant_id,
         EXTRACT(MONTH FROM psm.time) AS month,
@@ -54,6 +55,7 @@ TASK_C_VIEW_STATEMENTS = [
     FROM public.plant_sub_measurements psm
     JOIN public.energy_unit p ON p.id = psm.plant_id
     GROUP BY psm.plant_id, p.name, EXTRACT(MONTH FROM psm.time)
+    ;
 
     CREATE VIEW energy_flow AS
         SELECT ppp.name, ppp.month, ppp.total_production_kwh, ppp.total_substation_pwr_kwh, ed.delivered_pwr
@@ -61,6 +63,7 @@ TASK_C_VIEW_STATEMENTS = [
         JOIN public.energy_unit eu ON eu.name = ppp.name
         JOIN public.energy_delivered ed ON ed.pwr_plant_id = eu.id AND ed.month = ppp.month
         ORDER BY ppp.name, ppp.month
+    ;
     """,
 ]
 
